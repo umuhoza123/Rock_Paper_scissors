07a5e0d32eed4eaea78278f93cb94a86ba405b40
umuhoza123 <brandineumuhoza@gmail.com>
2023-07-08 19:31:09 +0200

    update script
    last

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let round = 1;
 
 function playGame(playerChoice) {
   const choices = ["rock", "paper", "scissors"];
@@ -12,6 +13,15 @@ function playGame(playerChoice) {
   document.getElementById("game-result").textContent = result;
 
   updateScore(result);
+
+  if (round === 5) {
+    determineFinalWinner();
+  } else {
+    round++;
+    setTimeout(() => {
+      document.getElementById("game-result").textContent = "Choose your move for round " + round;
+    }, 2000);
+  }
 }
 
 function determineWinner(playerChoice, computerChoice) {
@@ -35,4 +45,19 @@ function determineWinner(playerChoice, computerChoice) {
 function updateScore(result) {
   document.getElementById("player-score").textContent = playerScore;
   document.getElementById("computer-score").textContent = computerScore;
+}
+
+function determineFinalWinner() {
+  if (playerScore > computerScore) {
+    document.getElementById("game-result").textContent = "You win the game!";
+  } else if (playerScore < computerScore) {
+    document.getElementById("game-result").textContent = "Computer wins the game!";
+  } else {
+    document.getElementById("game-result").textContent = "It's a tie! The game is a draw.";
+  }
+
+  // Reset the game for a new round
+  playerScore = 0;
+  computerScore = 0;
+  round = 1;
 }
